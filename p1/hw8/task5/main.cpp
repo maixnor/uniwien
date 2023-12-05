392c347cbbbc5ef1c557072d833d4a491f1e8462
maixnor <46966993+maixnor@users.noreply.github.com>
2023-12-05 05:18:08 +0100

    hw8 post gpt, fixed everything

--- p1/hw8/task5/main.cpp
+++ p1/hw8/task5/main.cpp
@@ -3,61 +3,60 @@
 using namespace std;
 
 class Piggy {
-	private:
-		int cents;
-		int nickels;
-		int dimes;
+private:
+    int cents;
+    int nickels;
+    int dimes;
 
-		int total() {
-			return cents + nickels * 5 + dimes * 10;
-		}
+    int total() const {
+        return cents + nickels * 5 + dimes * 10;
+    }
 
-	public:
-		Piggy() {
-			cents = 0;
-			nickels = 0;
-			dimes = 0;
-		}
+public:
+    Piggy() : cents(0), nickels(0), dimes(0) {}
 
-		void empty() {
-  		cents, nickels, dimes = 0;
-		}
+    void empty() {
+        cents = nickels = dimes = 0;
+    }
 
-		void insert(int a, int b, int c) {
-			cents += a;
-			nickels += b;
-			dimes += c;
-		}
+    void insert(int a, int b, int c) {
+        cents += a;
+        nickels += b;
+        dimes += c;
+    }
 
-		void print() {
-			cout << "cents: " << cents << ", nickels: " << nickels << ", dimes: " << dimes << endl;
-		 	cout << "total amount in piggy bank: " << total() << endl;
-		}
+    void print() const {
+        cout << "cents: " << cents << ", nickels: " << nickels << ", dimes: " << dimes << endl;
+        cout << "total amount in piggy bank: " << total() << endl;
+    }
 
-		inline bool operator< (const Piggy& lhs, const Piggy& rhs) { return lhs.total() < rhs.total() }
-		inline bool operator> (const Piggy& lhs, const Piggy& rhs) { return rhs < lhs; }
-		inline bool operator<=(const Piggy& lhs, const Piggy& rhs) { return !(lhs > rhs); }
-		inline bool operator>=(const Piggy& lhs, const Piggy& rhs) { return !(lhs < rhs); }
-
-}
+    bool operator<(const Piggy& rhs) const { return total() < rhs.total(); }
+    bool operator>(const Piggy& rhs) const { return total() > rhs.total(); }
+    bool operator<=(const Piggy& rhs) const { return total() <= rhs.total(); }
+    bool operator>=(const Piggy& rhs) const { return total() >= rhs.total(); }
+    bool operator==(const Piggy& rhs) const { return total() == rhs.total(); }
+    bool operator!=(const Piggy& rhs) const { return total() != rhs.total(); }
+};
 
 int main() {
+    Piggy piggy1;
+    Piggy piggy2;
 
-	Piggy piggy1;
-	Piggy piggy2;
+    piggy1.insert(0, 2, 0);
+    cout << (piggy1 > piggy2) << endl;
+    piggy2.insert(0, 0, 2);
+    cout << (piggy1 < piggy2) << endl;
+    cout << (piggy1 != piggy2) << endl;
+    piggy1.insert(10, 0, 0);
+    cout << (piggy1 == piggy2) << endl;
+    piggy1.print();
+    piggy2.print();
 
-	piggy1.insert(0, 2, 0);
-	cout << piggy1 > piggy2 << endl;
-	piggy2.insert(0, 0, 2);
-	cout << piggy1 < piggy2 << endl;
-	cout << piggy1 != piggy2 << endl;
-	piggy1.insert(10, 0, 0);
-	cout << piggy1 == piggy2 << endl;
-	piggy1.print();
-	piggy2.print();
+    piggy1.empty();
+    piggy2.empty();
 
-	piggy1.empty();
-	piggy2.empty();
+		piggy1.print();
 
-	return 0;
+    return 0;
 }
+
