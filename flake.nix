f1429c0d9c42614e38fc4aa6085bbee6eadba977
maixnor <46966993+maixnor@users.noreply.github.com>
2024-03-08 15:43:37 +0100

    snapshot

--- flake.nix
+++ flake.nix
@@ -1,38 +1,62 @@
 {
-  description = "A development environment for Jupyter notebooks with Python";
+  description = "A development environment for Jupyter notebooks with Python and R";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-        myPythonEnv = pkgs.python3.withPackages (ps: with ps; [
-          ipython
-          notebook
-          jupyterlab
-          numpy
-          pandas
-          matplotlib
-          scipy
-        ]);
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [ myPythonEnv pkgs.vscode pkgs.vscode-extensions.ms-python.python ];
-          shellCmd = ''
+        myEnv = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+              ipython
+              notebook
+              jupyterlab
+              numpy
+              pandas
+              matplotlib
+              scipy
+							sympy
+              # Add any additional Python packages here
+            ]))
+            R
+            rPackages.IRkernel
+            vscode
+            # Add any R packages here, e.g., rPackages.ggplot2, rPackages.dplyr, etc.
+          ];
+          shellHook = ''
 						git pull --all
-            code notebooks
-						git add .
-						git commit -m "snapshot $(date)"
-						git push
+
+            export PYTHONPATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
+            export JUPYTER_PATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
+            echo "Starting Jupyter server without a password..."
+            jupyter lab --no-browser --ip=127.0.0.1 --port=8888 --NotebookApp.token="" &
+            JUPYTER_PID=$!
+            echo "Jupyter server started with PID $JUPYTER_PID"
+            function cleanup {
+              echo "Stopping Jupyter server..."
+              kill $JUPYTER_PID
+
+							git add .
+							git commit -m "snapshot"
+							git push
+            }
+            trap cleanup EXIT
+            # Register IRkernel if not already done
+            Rscript -e "IRkernel::installspec(user = FALSE)" || true
+
+						code ./notebooks
           '';
         };
+      in
+      {
+        devShell = myEnv;
       });
 }
 
