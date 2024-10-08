1cb090fd346d3007501482db9ed5555edee888ac
maixnor <46966993+maixnor@users.noreply.github.com>
2024-10-08 04:28:56 +0200

    cleanup, no more direnv

--- notebooks/flake.nix
+++ notebooks/flake.nix
@@ -6,57 +6,66 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-				pkgs = import nixpkgs { 
-					inherit system;
+        pkgs = import nixpkgs {
+          inherit system;
 
-					config = {
-						allowUnfree = true;
-					};
-				};
+          config = {
+            allowUnfree = true;
+          };
+        };
         jupyterEnv = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (python3.withPackages (ps: with ps; [
-              ipython
-              notebook
-              jupyterlab
-              numpy
-              pandas
-              matplotlib
-							sympy
-              # Add any additional Python packages here
-            ]))
+            (python3.withPackages (
+              ps: with ps; [
+                ipython
+                notebook
+                jupyterlab
+                numpy
+                pandas
+                matplotlib
+                sympy
+                # Add any additional Python packages here
+              ]
+            ))
             vscodium
           ];
           shellHook = ''
-						git pull --all
+            						git pull --all
 
-            export PYTHONPATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
-            export JUPYTER_PATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
-            echo "Starting Jupyter server without a password..."
-            jupyter lab --no-browser --ip=127.0.0.1 --port=8888 --NotebookApp.token="" &
-            JUPYTER_PID=$!
-            echo "Jupyter server started with PID $JUPYTER_PID"
-            function cleanup {
-              echo "Stopping Jupyter server..."
-              kill $JUPYTER_PID
+                        export PYTHONPATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
+                        export JUPYTER_PATH="${pkgs.python3}/${pkgs.python3.sitePackages}"
+                        echo "Starting Jupyter server without a password..."
+                        jupyter lab --no-browser --ip=127.0.0.1 --port=8888 --NotebookApp.token="" &
+                        JUPYTER_PID=$!
+                        echo "Jupyter server started with PID $JUPYTER_PID"
+                        function cleanup {
+                          echo "Stopping Jupyter server..."
+                          kill $JUPYTER_PID
 
-							git add ./notebooks
-							git commit -m "snapshot notebooks"
-            }
-            trap cleanup EXIT
+            							git add ./notebooks
+            							git commit -m "snapshot notebooks"
+                        }
+                        trap cleanup EXIT
 
-						# --wait for the process to not exit immediately
-						codium --install-extension ms-toolsai.jupyter
-						codium . --wait
-						exit
+            						# --wait for the process to not exit immediately
+            						codium --install-extension ms-toolsai.jupyter
+            						codium . --wait
+            						exit
           '';
         };
       in
       {
         devShell = jupyterEnv;
-      });
+      }
+    );
 }
-
