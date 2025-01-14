7b0c71e71e384679352c80ed00c2740f472f9e2f
maixnor <46966993+maixnor@users.noreply.github.com>
2025-01-14 05:55:16 +0100

    snapshot notebooks

--- notebooks/flake.nix
+++ notebooks/flake.nix
@@ -22,33 +22,10 @@
             allowUnfree = true;
           };
         };
-        jupyter = import (pkgs.fetchFromGitHub {                                      
-          owner = "tweag";                                                            
-          repo = "jupyterWith";                         
-          # Replace this with current revision.                              
-          rev = "269999caa8d506e93ff56c8643cecb91ded2fdef";                           
-          sha256 = "08iig872ay8d711n2gbfzrf496m9x9a9xwr0xca9hn7j61c3xr43";            
-          fetchSubmodules = true;                                                     
-        }) {};                                                                        
-                                                                                      
-        kernels = jupyter.kernels;                                                    
-                                                                                      
-        irkernel = kernels.iRWith {                
-          name = "nixpkgs";                                                         
-          # Libraries to be available to the kernel.                                
-          packages = p: with p; [                                                   
-            ggplot2                                                          
-          ];                                           
-        };                                                                          
-                                                                                      
-        jupyterEnvironment = (jupyter.jupyterlabWith {                                
-          kernels = [ irkernel ];                                                   
-        });                                                                         
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            jupyterEnvironment.env
             (python3.withPackages (
               ps: with ps; [
                 ipython
